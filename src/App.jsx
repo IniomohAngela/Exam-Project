b9f59c666d921c95d7cfeefd720f3b58bc331b4d
Iniomoh Angela <angela.iniomoh@gmail.com>
2022-11-14 23:50:32 +0000

    update src

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Pages/Home/Home';
-import Repolist from './components/Pages/Repo/Repolist'
-import Repo from './components/Pages/Repo/Repo';
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import About from './components/Pages/About/About';
-import Error from './components/Pages/Error/Error';
+
 
 const App = () => {
   return (
@@ -15,10 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/repos">
-            <Route index element={<Repolist />} />
-             <Route path=":repo" element={<Repo />} />
-          </Route>
+
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Repos.jsx
+++
@@ -1,4 +0,0 @@
-
-function Repos() {
-}
-export default Repos;
